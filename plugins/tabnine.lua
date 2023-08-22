06f1bcdffcee5f479fe179541084fcb85220cc65
xoffio <marquessricardo@gmail.com>
2023-08-21 22:50:33 -0400

    tabnine added

---
+++ plugins/tabnine.lua
@@ -0,0 +1,25 @@
+-- Get platform dependant build script
+local function tabnine_build_path()
+    if vim.loop.os_uname().sysname == "Windows_NT" then
+        return "powershell.exe -file .\\dl_binaries.ps1"
+    else
+        return "./dl_binaries.sh"
+    end
+end
+
+return  {
+    "codota/tabnine-nvim",
+    build = tabnine_build_path(),
+    name = "tabnine",
+    cmd = { "TabnineStatus", "TabnineDisable", "TabnineEnable", "TabnineToggle" },
+    event = "User AstroFile",
+    opts = {
+        disable_auto_comment=true,
+        accept_keymap="<C-e>",
+        dismiss_keymap = "<C><BS>",
+        debounce_ms = 800,
+        suggestion_color = {gui = "#808080", cterm = 244},
+        exclude_filetypes = {"TelescopePrompt"},
+        log_file_path = nil, -- absolute path to Tabnine log file
+    },
+}
