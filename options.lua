ab7baaacffb1c67513c21adf853b2a836e9aa37c
Xoffio <38369407+Xoffio@users.noreply.github.com>
2024-01-21 15:18:22 -0500

    relative number enabled
    I am ready to use relative number lines

--- options.lua
+++ options.lua
@@ -2,7 +2,7 @@
 return {
   opt = {
     -- set to true or false etc.
-    relativenumber = false, -- sets vim.opt.relativenumber
+    relativenumber = true, -- sets vim.opt.relativenumber
     number = true,          -- sets vim.opt.number
     spell = true,           -- sets vim.opt.spell
     signcolumn = "auto",    -- sets vim.opt.signcolumn to auto
