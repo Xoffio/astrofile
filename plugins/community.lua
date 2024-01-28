38a2df464c99bdee035ced7da245f1c4288b1910
xoffio <marquessricardo@gmail.com>
2024-01-28 18:04:47 -0500

    adding typescript, html, css, and tailwind support via astrocommunity

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,9 +4,12 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  { import = "astrocommunity.colorscheme.catppuccin", enabled = true },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.typescript", enabled = true },
+  { import = "astrocommunity.pack.html-css", enabled = true },
+  { import = "astrocommunity.pack.tailwindcss", enabled = true },
+  { import = "astrocommunity.colorscheme.catppuccin", enabled = true },
   {
     -- further customize the options set by the community
     "catppuccin",
