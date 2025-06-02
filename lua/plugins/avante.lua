f1ba4fdc13b7eff1c225657f8ff74cbdaca9af4e
xoffio <38369407+Xoffio@users.noreply.github.com>
2025-06-02 09:01:10 -0400

    Initial version for Avante

---
+++ lua/plugins/avante.lua
@@ -0,0 +1,26 @@
+local sysname = vim.uv.os_uname().sysname
+local is_win = sysname == "Windows_NT"
+
+return {
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    version = false, -- Never set this value to "*"! Never!
+    opts = {
+      -- add any opts here
+      -- for example
+      provider = "ollama",
+      ollama = {
+        model = "qwen2.5-coder:3b",
+      },
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = is_win and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" or "make",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+    },
+  },
+}
