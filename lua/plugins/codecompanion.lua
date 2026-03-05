52c8d6a8227ec309531e66ed09790bb6cb246c98
xoffio <marquessricardo@gmail.com>
2026-03-04 22:42:18 -0500

    Codecompanion using copilot

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -1,41 +1,43 @@
 -- NOTE: uncomment the next line to disable this plugin
 if true then return {} end
 
-local sysname = vim.uv.os_uname().sysname
-local is_win = sysname == "Windows_NT"
+-- local sysname = vim.uv.os_uname().sysname
+-- local is_win = sysname == "Windows_NT"
 
 return {
   {
     "olimorris/codecompanion.nvim",
+    version = "^19.0.0",
     opts = {
-      adapters = {
-        ollama = function()
-          return require("codecompanion.adapters").extend("ollama", {
-            env = {
-              url = "http://localhost:11434",
-              api_key = is_win and "APPDATA" or "TERM",
-            },
-            headers = {
-              ["Content-Type"] = "application/json",
-              ["Authorization"] = "Bearer ${api_key}",
-            },
-            parameters = {
-              sync = true,
-            },
-          })
-        end,
-      },
-      strategies = {
-        chat = {
-          adapter = "ollama",
-        },
-        inline = {
-          adapter = "ollama",
-        },
-        cmd = {
-          adapter = "ollama",
-        },
-      },
+      -- Keep this commented if using copilot.
+      -- adapters = {
+      --   ollama = function()
+      --     return require("codecompanion.adapters").extend("ollama", {
+      --       env = {
+      --         url = "http://localhost:11434",
+      --         api_key = is_win and "APPDATA" or "TERM",
+      --       },
+      --       headers = {
+      --         ["Content-Type"] = "application/json",
+      --         ["Authorization"] = "Bearer ${api_key}",
+      --       },
+      --       parameters = {
+      --         sync = true,
+      --       },
+      --     })
+      --   end,
+      -- },
+      -- strategies = {
+      --   chat = {
+      --     adapter = "ollama",
+      --   },
+      --   inline = {
+      --     adapter = "ollama",
+      --   },
+      --   cmd = {
+      --     adapter = "ollama",
+      --   },
+      -- },
     },
     dependencies = {
       "nvim-lua/plenary.nvim",
